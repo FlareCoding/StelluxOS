9d757bc3078455d0df10a5b3393c61ec04a55c07
FlareCoding <albert.slepak@gmail.com>
2023-10-22 23:47:59 -0400

    temporary disabled spinlocks to help debug a preemption bug

--- kernel/src/entry/kernel_entry.cpp
+++ kernel/src/entry/kernel_entry.cpp
@@ -37,15 +37,15 @@ int fibb(int n);
 // use recursive function to exercise context switch (fibb)
 void simple_function() {
     while(1) {
-        int result = fibb(36);
-        kuPrint("simple_function>  fibb(36): %i\n", result);
+        int result = fibb(40);
+        kuPrint("simple_function>  fibb(40): %i\n", result);
     }
 }
 
 void simple_function2() {
     while(1) {
-        int result = fibb(34);
-        kuPrint("simple_function2> fibb(34): %i\n", result);
+        int result = fibb(38);
+        kuPrint("simple_function2> fibb(38): %i\n", result);
     }
 }
 
