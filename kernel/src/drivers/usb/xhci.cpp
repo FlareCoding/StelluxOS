776f1c7cb570c7668fa197367ce2ba60e5acb682
FlareCoding <albert.slepak@gmail.com>
2024-07-22 20:05:47 -0400

    implemented parsing the most important capability register fields

--- kernel/src/drivers/usb/xhci.cpp
+++ kernel/src/drivers/usb/xhci.cpp
@@ -18,8 +18,54 @@ namespace drivers {
     bool XhciDriver::init(PciDeviceInfo& deviceInfo) {
         _mapDeviceMmio(deviceInfo.barAddress);
 
-        kuPrint("\n");
+        _parseCapabilityRegisters();
+        _logCapabilityRegisters();
+
+        // m_opRegs = reinterpret_cast<volatile XhciOperationalRegisters*>(m_xhcBase + capLen);
+        // kprintInfo("usbsts: 0x%x\n", m_opRegs->usbsts);
+
+        kprint("\n");
         return true;
+    }
+
+    void XhciDriver::_parseCapabilityRegisters() {
+        m_capRegs = reinterpret_cast<volatile XhciCapabilityRegisters*>(m_xhcBase);
+
+        m_capabilityRegsLength = m_capRegs->caplength;
+
+        m_maxDeviceSlots = XHCI_MAX_DEVICE_SLOTS(m_capRegs);
+        m_maxInterrupters = XHCI_MAX_INTERRUPTERS(m_capRegs);
+        m_maxPorts = XHCI_MAX_PORTS(m_capRegs);
+
+        m_isochronousSchedulingThreshold = XHCI_IST(m_capRegs);
+        m_erstMax = XHCI_ERST_MAX(m_capRegs);
+        m_maxScratchpadBuffers = XHCI_MAX_SCRATCHPAD_BUFFERS(m_capRegs);
+
+        m_64bitAddressingCapability = XHCI_AC64(m_capRegs);
+        m_bandwidthNegotiationCapability = XHCI_BNC(m_capRegs);
+        m_64ByteContextSize = XHCI_CSZ(m_capRegs);
+        m_portPowerControl = XHCI_PPC(m_capRegs);
+        m_portIndicators = XHCI_PIND(m_capRegs);
+        m_lightResetCapability = XHCI_LHRC(m_capRegs);
+        m_extendedCapabilitiesPointer = XHCI_XECP(m_capRegs);
+    }
+
+    void XhciDriver::_logCapabilityRegisters() {
+        kprintInfo("===== Capability Registers (0x%llx) =====\n", (uint64_t)m_capRegs);
+        kprintInfo("    Length                : %i\n", m_capabilityRegsLength);
+        kprintInfo("    Max Device Slots      : %i\n", m_maxDeviceSlots);
+        kprintInfo("    Max Interrupters      : %i\n", m_maxInterrupters);
+        kprintInfo("    Max Ports             : %i\n", m_maxPorts);
+        kprintInfo("    IST                   : %i\n", m_isochronousSchedulingThreshold);
+        kprintInfo("    ERST Max Size         : %i\n", m_erstMax);
+        kprintInfo("    Scratchpad Buffers    : %i\n", m_maxScratchpadBuffers);
+        kprintInfo("    64-bit Addressing     : %i\n", m_64bitAddressingCapability);
+        kprintInfo("    Bandwidth Negotiation : %i\n", m_bandwidthNegotiationCapability);
+        kprintInfo("    64-byte Context Size  : %i\n", m_64ByteContextSize);
+        kprintInfo("    Port Power Control    : %i\n", m_portPowerControl);
+        kprintInfo("    Port Indicators       : %i\n", m_portIndicators);
+        kprintInfo("    Light Reset Available : %i\n", m_lightResetCapability);
+        kprint("\n");
     }
 
     void XhciDriver::_mapDeviceMmio(uint64_t pciBarAddress) {
