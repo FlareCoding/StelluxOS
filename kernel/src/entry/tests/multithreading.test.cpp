58559493371c558e79e6aac430c66aaaf69d78a4
FlareCoding <albert.slepak@gmail.com>
2024-09-10 15:07:46 -0400

    thanks to the unit testing framework, a critical bug of the kernel heap region not getting properly locked by the page frame allocator has now been fixed

--- kernel/src/entry/tests/multithreading.test.cpp
+++ kernel/src/entry/tests/multithreading.test.cpp
@@ -14,33 +14,18 @@ void incrementMtUnitTestCounter() {
     exitKernelThread();
 }
 
-// DECLARE_UNIT_TEST("Multithreading Test - Kernel Task Creation", mtTaskCreationUnitTest) {
-//     const size_t iterations = 10000;
-//     const size_t milestone = 1000;
+DECLARE_UNIT_TEST("Multithreading Test - Kernel Task Creation", mtTaskCreationUnitTest) {
+    const size_t iterations = 10000;
+    const size_t milestone = 1000;
 
-//     for (size_t i = 1; i <= iterations; i++) {
-//         // Task* task = createKernelTask(incrementMtUnitTestCounter);
-//         Task* task = (Task*)kmalloc(sizeof(Task));
-//         if (!task) {
-//             kuPrint(UNIT_TEST "Failed to create kernel task on iteration %lli\n", i);
-//             return UNIT_TEST_FAILURE;
-//         }
-//         // zeromem(task, sizeof(Task));
+    for (size_t i = 1; i <= iterations; i++) {
+        Task* task = createKernelTask(incrementMtUnitTestCounter);
+        ASSERT_TRUE(task, "Failed to allocate a kernel task");
 
-//         // Allocate both user and kernel stacks
-//         void* userStack = zallocPages(2);
-//         void* kernelStack = zallocPages(2);
-//         if (!userStack || !kernelStack) {
-//             kuPrint(UNIT_TEST "Failed to create user or kernel stack on iteration %lli\n", i);
-//             return UNIT_TEST_FAILURE;
-//         }
+        if (i % milestone == 0) {
+            kuPrint(UNIT_TEST "Allocated %lli tasks\n", i);
+        }
+    }
 
-//         ASSERT_TRUE(task, "Failed to create kernel task");
-
-//         if (i % milestone == 0) {
-//             kuPrint(UNIT_TEST "Allocated %lli tasks\n", i);
-//         }
-//     }
-
-//     return UNIT_TEST_SUCCESS;
-// }
+    return UNIT_TEST_SUCCESS;
+}
