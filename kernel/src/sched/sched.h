f1049773cff02e532a2f1b90795e28cb1011629b
FlareCoding <albert.slepak@gmail.com>
2024-09-11 14:59:01 -0400

    provided better scheduler documentation

--- kernel/src/sched/sched.h
+++ kernel/src/sched/sched.h
@@ -20,33 +20,127 @@ public:
     Scheduler() = default;
     ~Scheduler() = default;
 
+    /**
+     * @brief Returns the singleton instance of the Scheduler.
+     * 
+     * @return Scheduler& Reference to the Scheduler instance.
+     */
     static Scheduler& get();
 
+    /**
+     * @brief Initializes the Scheduler system.
+     * 
+     * This function sets up necessary data structures and prepares 
+     * the Scheduler to manage tasks across multiple CPU cores.
+     */
     void init();
+
+    /**
+     * @brief Registers a CPU core for task scheduling.
+     * 
+     * This function marks a given CPU core as eligible for task scheduling. 
+     * This ensures that tasks can be assigned and managed on this core.
+     * 
+     * @param cpu The CPU core number to register.
+     */
     void registerCoreForScheduling(int cpu);
 
-    // Get the current running task
+    /**
+     * @brief Retrieves the currently running task on the specified CPU.
+     * 
+     * This function returns a pointer to the task that is currently 
+     * being executed on the specified CPU core.
+     * 
+     * @param cpu The CPU core number to query.
+     * @return Task* Pointer to the currently running task on the specified CPU.
+     */
     Task* getCurrentTask(int cpu);
 
-    // Look at the next available task
+    /**
+     * @brief Peeks at the next task in the queue for the specified CPU.
+     * 
+     * This function returns a pointer to the next task that is 
+     * scheduled to run on the specified CPU core without actually 
+     * switching to it. This allows you to inspect the task that 
+     * will be executed next.
+     * 
+     * @param cpu The CPU core number to query.
+     * @return Task* Pointer to the next task in the queue for the specified CPU.
+     */
     Task* peekNextTask(int cpu);
 
-    // Schedule the next task to run
+    /**
+     * @brief Schedules the next task to run on the specified CPU.
+     * 
+     * This function performs the task switch for the given CPU core. 
+     * It moves the next task in the queue to the running state and 
+     * ensures that the previous task is stopped or moved to the 
+     * appropriate state (e.g., waiting).
+     * 
+     * @param cpu The CPU core number to schedule the next task on.
+     */
     void scheduleNextTask(int cpu);
 
-    // Add a task to a specified cpu core
+    /**
+     * @brief Adds a task to a specific CPU core's run queue.
+     * 
+     * This function assigns a task to the specified CPU's scheduling 
+     * queue. The task will eventually be scheduled and executed by 
+     * the Scheduler on that core.
+     * 
+     * @param task Pointer to the task to be scheduled.
+     * @param cpu The CPU core number to add the task to.
+     */
     void addTaskToCpu(Task* task, int cpu);
 
-    // Adds a task to the next least loaded available cpu core
+    /**
+     * @brief Adds a task to the least loaded CPU core.
+     * 
+     * This function examines the load on all available CPU cores and 
+     * assigns the task to the core that has the fewest running tasks 
+     * or lowest load. It provides a simple form of load balancing 
+     * to distribute tasks efficiently.
+     * 
+     * @param task Pointer to the task to be scheduled.
+     */
     void addTask(Task* task);
 
-    // Remove a task
+    /**
+     * @brief Removes a task from a specific CPU core's run queue.
+     * 
+     * This function removes the task with the given process ID (PID) 
+     * from the specified CPU's scheduling queue. This may occur if 
+     * the task has completed execution or has been explicitly stopped.
+     * 
+     * @param pid The process ID of the task to be removed.
+     * @param cpu The CPU core number from which to remove the task.
+     */
     void removeTaskFromCpu(int pid, int cpu);
 
 private:
+    // List of run queues for each CPU core, holding tasks to be scheduled on that core
     kstl::vector<kstl::SharedPtr<SchedulerRunQueue>> m_runQueues;
 
+    /**
+     * @brief Finds the least loaded CPU core.
+     * 
+     * This function inspects the run queues of all registered CPU cores 
+     * and returns the core number of the least loaded one. This is 
+     * typically used for load balancing when adding new tasks.
+     * 
+     * @return int The CPU core number with the least load.
+     */
     int _findLeastLoadedCpu();
+
+    /**
+     * @brief Retrieves the size of the run queue for the specified CPU.
+     * 
+     * This function returns the number of tasks currently in the scheduling 
+     * queue for the specified CPU core, providing insight into the load on that core.
+     * 
+     * @param cpu The CPU core number to query.
+     * @return size_t The number of tasks in the run queue for the specified CPU.
+     */
     size_t _getRunQueueSize(size_t cpu);
 };
 
