5de988fc8f0419f08e2ed157019f868e50b0e1bf
FlareCoding <albert.slepak@gmail.com>
2024-09-24 12:58:29 -0400

    reworked the detection of MSI/MSI-X PCI capabilities

--- kernel/src/acpi/mcfg.h
+++ kernel/src/acpi/mcfg.h
@@ -9,36 +9,7 @@ struct McfgHeader {
     uint64_t reserved;
 }__attribute__((packed));
 
-struct PciDeviceInfo {
-    PciDeviceHeader headerInfo;
-    uint64_t        functionAddress;
-    uint64_t        barAddress;
-    uint8_t         bus;
-    uint8_t         device;
-    uint8_t         function;
-    uint8_t         padding;
-    uint32_t        capabilities;
-};
-
 #define HAS_PCI_CAP(info, cap) ((bool)(info.capabilities & (1u << cap)))
-
-struct PciMsiXCapability {
-    uint16_t messageControl;
-    uint32_t tableOffset;
-    uint32_t pbaOffset; // PBA: Pending Bit Array
-} __attribute__((packed));
-
-struct MsiXTableEntry {
-    uint64_t messageAddress;
-    uint32_t messageData;
-    uint32_t vectorControl;
-} __attribute__((packed));
-
-struct PciMsiCapability {
-    uint16_t messageControl;
-    uint32_t messageAddress;
-    uint16_t messageData;
-} __attribute__((packed));
 
 class Mcfg {
 public:
@@ -54,8 +25,6 @@ public:
     // Helper functions for finding popular needed PCI devices
     size_t findXhciController();
     size_t findEhciController();
-
-    __PRIVILEGED_CODE void checkInterruptMechanism(uint8_t bus, uint8_t device, uint8_t function);
 
 private:
     McfgHeader* m_base;
@@ -74,11 +43,5 @@ private:
     __PRIVILEGED_CODE
     uint32_t _readCapabilities(uint8_t bus, uint8_t device, uint8_t function);
 };
-
-__PRIVILEGED_CODE
-PciMsiXCapability _readMsixCapability(const uint8_t bus, const uint8_t device, const uint8_t function, uint32_t& capOffset);
-
-__PRIVILEGED_CODE
-PciMsiCapability _readMsiCapability(const uint8_t bus, const uint8_t device, const uint8_t function, uint32_t& capOffset);
 
 #endif
