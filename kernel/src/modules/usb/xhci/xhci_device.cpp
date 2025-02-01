91e87fed8ccd3057379f88889ff57c1c90f4c70b
FlareCoding <albert.slepak@gmail.com>
2025-02-01 13:25:32 -0500

    refactored xhci device and endpoint classes and routines

--- kernel/src/modules/usb/xhci/xhci_device.cpp
+++ kernel/src/modules/usb/xhci/xhci_device.cpp
@@ -1,121 +1,85 @@
 #include <modules/usb/xhci/xhci_device.h>
-#include <memory/paging.h>
 
-uint8_t get_endpoint_type_from_ep_descriptor(usb_endpoint_descriptor* desc) {
-    uint8_t endpoint_direction_in = (desc->bEndpointAddress & 0x80) ? 1 : 0;
-    uint8_t transfer_type = desc->bmAttributes & 0x3;
-
-    switch (transfer_type) {
-    case 0: {
-        return XHCI_ENDPOINT_TYPE_CONTROL;
-    }
-    case 1: {
-        return endpoint_direction_in ? XHCI_ENDPOINT_TYPE_ISOCHRONOUS_IN : XHCI_ENDPOINT_TYPE_ISOCHRONOUS_OUT;
-    }
-    case 2: {
-        return endpoint_direction_in ? XHCI_ENDPOINT_TYPE_BULK_IN : XHCI_ENDPOINT_TYPE_BULK_OUT;
-    }
-    case 3: {
-        return endpoint_direction_in ? XHCI_ENDPOINT_TYPE_INTERRUPT_IN : XHCI_ENDPOINT_TYPE_INTERRUPT_OUT;
-    }
-    default: break;
-    }
-
-    return 0;
+xhci_device::xhci_device(uint8_t port, uint8_t slot, uint8_t speed, bool use_64byte_ctx)
+    : m_port_id(port), m_slot_id(slot), m_speed(speed), m_use64byte_ctx(use_64byte_ctx) {
+    _alloc_input_ctx();
+    _alloc_ctrl_ep_ring();
 }
 
-xhci_device_endpoint_descriptor::xhci_device_endpoint_descriptor(
-    uint8_t slot_id,
-    uint8_t config_value,
-    usb_endpoint_descriptor* desc
-) : slot_id(slot_id), configuration_value(config_value) {
-    uint8_t endpoint_number_base = desc->bEndpointAddress & 0x0F;
-    uint8_t endpoint_direction_in = (desc->bEndpointAddress & 0x80) ? 1 : 0;
-
-    endpoint_num = (endpoint_number_base * 2) + endpoint_direction_in;
-    endpoint_type = get_endpoint_type_from_ep_descriptor(desc);
-    max_packet_size = desc->wMaxPacketSize;
-    interval = desc->bInterval;
-    
-    data_buffer = (uint8_t*)alloc_xhci_memory(desc->wMaxPacketSize, 64, 64);
-    transfer_ring = xhci_transfer_ring::allocate(slot_id);
-}
-
-xhci_device_endpoint_descriptor::~xhci_device_endpoint_descriptor() {
-    free_xhci_memory(data_buffer);
-}
-
-void xhci_device::allocate_input_context(bool use_64byte_contexts) {
+void xhci_device::_alloc_input_ctx() {
     // Calculate the input context size based
     // on the capability register parameters.
-    uint64_t input_context_size = use_64byte_contexts ? sizeof(xhci_input_context64) : sizeof(xhci_input_context32);
+    uint64_t input_context_size = m_use64byte_ctx ? sizeof(xhci_input_context64) : sizeof(xhci_input_context32);
 
-    // Allocate and zero out the input context
-    m_input_context = alloc_xhci_memory(
+    // Allocate the input context memory
+    m_input_ctx = alloc_xhci_memory(
         input_context_size,
         XHCI_INPUT_CONTROL_CONTEXT_ALIGNMENT,
         XHCI_INPUT_CONTROL_CONTEXT_BOUNDARY
     );
+
+    m_input_ctx_dma_addr = xhci_get_physical_addr(m_input_ctx);
 }
 
-uint64_t xhci_device::get_input_context_physical_base() {
-    return xhci_get_physical_addr(m_input_context);
+void xhci_device::_alloc_ctrl_ep_ring() {
+    m_ctrl_ep_ring = xhci_transfer_ring::allocate(m_slot_id);
 }
 
-void xhci_device::allocate_control_ep_transfer_ring() {
-    m_control_endpoint_transfer_ring = xhci_transfer_ring::allocate(slot_id);
-}
-
-xhci_input_control_context32* xhci_device::get_input_control_context(bool use_64byte_contexts) {
-    if (use_64byte_contexts) {
-        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_context);
+xhci_input_control_context32* xhci_device::get_input_ctrl_ctx() {
+    if (m_use64byte_ctx) {
+        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_ctx);
         return reinterpret_cast<xhci_input_control_context32*>(&input_ctx->control_context);
     } else {
-        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_context);
+        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_ctx);
         return &input_ctx->control_context;
     }
 }
 
-xhci_slot_context32* xhci_device::get_input_slot_context(bool use_64byte_contexts) {
-    if (use_64byte_contexts) {
-        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_context);
+xhci_slot_context32* xhci_device::get_input_slot_ctx() {
+    if (m_use64byte_ctx) {
+        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_ctx);
         return reinterpret_cast<xhci_slot_context32*>(&input_ctx->device_context.slot_context);
     } else {
-        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_context);
+        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_ctx);
         return &input_ctx->device_context.slot_context;
     }
 }
 
-xhci_endpoint_context32* xhci_device::get_input_control_ep_context(bool use_64byte_contexts) {
-    if (use_64byte_contexts) {
-        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_context);
+xhci_endpoint_context32* xhci_device::get_input_ctrl_ep_ctx() {
+    if (m_use64byte_ctx) {
+        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_ctx);
         return reinterpret_cast<xhci_endpoint_context32*>(&input_ctx->device_context.control_ep_context);
     } else {
-        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_context);
+        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_ctx);
         return &input_ctx->device_context.control_ep_context;
     }
 }
 
-xhci_endpoint_context32* xhci_device::get_input_ep_context(bool use_64byte_contexts, uint8_t endpoint_id) {
-    uint8_t endpoint_index = endpoint_id - 2;
+xhci_endpoint_context32* xhci_device::get_input_ep_ctx(uint8_t endpoint_num) {
+    uint8_t endpoint_index = endpoint_num - 2;
 
-    if (use_64byte_contexts) {
-        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_context);
+    if (m_use64byte_ctx) {
+        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_ctx);
         return reinterpret_cast<xhci_endpoint_context32*>(&input_ctx->device_context.ep[endpoint_index]);
     } else {
-        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_context);
+        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_ctx);
         return &input_ctx->device_context.ep[endpoint_index];
     }
 }
 
-void xhci_device::copy_output_device_context_to_input_device_context(bool use_64byte_contexts, void* output_device_context) {
-    if (use_64byte_contexts) {
-        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_context);
+void xhci_device::sync_input_ctx(void* out_ctx) {
+    if (m_use64byte_ctx) {
+        xhci_input_context64* input_ctx = static_cast<xhci_input_context64*>(m_input_ctx);
         xhci_device_context64* input_device_ctx = &input_ctx->device_context;
-        memcpy(input_device_ctx, output_device_context, sizeof(xhci_device_context64));
+        memcpy(input_device_ctx, out_ctx, sizeof(xhci_device_context64));
     } else {
-        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_context);
+        xhci_input_context32* input_ctx = static_cast<xhci_input_context32*>(m_input_ctx);
         xhci_device_context32* input_device_ctx = &input_ctx->device_context;
-        memcpy(input_device_ctx, output_device_context, sizeof(xhci_device_context32));
+        memcpy(input_device_ctx, out_ctx, sizeof(xhci_device_context32));
     }
+}
+
+void xhci_device::setup_add_interface(const usb_interface_descriptor* desc) {
+    auto iface = kstl::make_shared<xhci_usb_interface>(m_slot_id, desc);
+    interfaces.push_back(iface);
 }
