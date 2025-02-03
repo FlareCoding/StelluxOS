b1d0b27a2bf50e781244bcebf2153bb0da4886ca
FlareCoding <albert.slepak@gmail.com>
2025-02-03 10:29:37 -0500

    fixed the priorities for using MSI-X vs legacy irq lines depending on whether QEMU environment is detected

--- kernel/src/modules/pci_module_base.cpp
+++ kernel/src/modules/pci_module_base.cpp
@@ -3,6 +3,10 @@
 #include <dynpriv/dynpriv.h>
 #include <serial/serial.h>
 
+#ifdef ARCH_X86_64
+#include <arch/x86/cpuid.h>
+#endif
+
 namespace modules {
 pci_module_base::pci_module_base(
     const kstl::string& name
@@ -23,27 +27,33 @@ void pci_module_base::attach_device(
             dev->enable_bus_mastering();
         }
 
+        bool m_qemu_detected = false;
+
+#ifdef ARCH_X86_64
+        m_qemu_detected = arch::x86::cpuid_is_running_under_qemu();
+#endif
+
         // Check if the device requires an IRQ vector
         uint8_t legacy_irq_line = dev->legacy_irq_line();
 
-        if (legacy_irq_line != 255 && legacy_irq_line != 0) {
-            // Allocate a free IRQ vector
-            m_irq_vector = find_free_irq_vector();
-
-            // Route the legacy IRQ line to the allocated IRQ vector
-            route_legacy_irq(legacy_irq_line, m_irq_vector, 0, IRQ_LEVEL_TRIGGERED);
-        } else if (dev->has_capability(pci::capability_id::msi_x)) {
+        if (dev->has_capability(pci::capability_id::msi_x) && !m_qemu_detected) {
             // Allocate a free IRQ vector
             m_irq_vector = find_free_irq_vector();
 
             // Setup MSI-X
             dev->setup_msix(0, m_irq_vector);
-        } else if (dev->has_capability(pci::capability_id::msi)) {
+        } else if (dev->has_capability(pci::capability_id::msi) && !m_qemu_detected) {
             // Allocate a free IRQ vector
             m_irq_vector = find_free_irq_vector();
 
             // Setup MSI
             dev->setup_msi(0, m_irq_vector);
+        } else if (legacy_irq_line != 255 && legacy_irq_line != 0) {
+            // Allocate a free IRQ vector
+            m_irq_vector = find_free_irq_vector();
+
+            // Route the legacy IRQ line to the allocated IRQ vector
+            route_legacy_irq(legacy_irq_line, m_irq_vector, 0, IRQ_LEVEL_TRIGGERED);
         }
     });
 }
