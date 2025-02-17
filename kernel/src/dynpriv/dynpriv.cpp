e30250fac4dd29a6829d336e503029b1478f36cf
FlareCoding <albert.slepak@gmail.com>
2025-02-17 18:36:46 -0500

    fixed dynamic privilege lower() inline assembly to not break under higher optimization levels by removing assumptions about the current_task offset

--- kernel/src/dynpriv/dynpriv.cpp
+++ kernel/src/dynpriv/dynpriv.cpp
@@ -92,12 +92,11 @@ void lower() {
         "popq %%r11;"            // Pop EFLAGS into r11 (as required by SYSRET)
         "cli;"                   // Disable interrupts
         "lea 1f(%%rip), %%rcx;"  // Load the address of the next instruction into rcx
-        "movq %%gs:0x0, %%rax;"  // Move the address of current task into rax
-        "btrq $0, 0x100(%%rax);" // Set current->elevated to 0
+        "btrq $0, 0x100(%0);"    // Set current->elevated to 0
         "sysretq;"               // Execute SYSRET and IF flag will get reset from the eflags
         "1:"                     // Label for the next instruction after SYSRET
         : /* no outputs */
-        : /* no inputs */
+        : "r"(current)
         : "rcx", "r11", "memory"   // Clobbered registers
     );
 }
@@ -108,11 +107,11 @@ void lower(void* target_fn) {
         "popq %%r11;"                        // Pop RFLAGS into R11 (required by SYSRETQ)
         "cli;"                               // Disable interrupts
         "mov %0, %%rcx;"                     // Move target_fn into RCX
-        "movq %%gs:0x0, %%rax;"              // Move the address of current task into rax
-        "btrq $0, 0x100(%%rax);"             // Set current->elevated to 0
+        "btrq $0, 0x100(%0);"                // Set current->elevated to 0
         "sysretq;"                           // Execute SYSRETQ to return to target_fn
         :
-        : "r" (target_fn)                    // Input operand: target_fn in any general-purpose register
+        : "r"(current),                      // %0 = pointer to current TCB
+          "r"(target_fn)                     // %1 = the target function
         : "rcx", "r11", "rax", "memory"      // Clobbered registers
     );
 }
