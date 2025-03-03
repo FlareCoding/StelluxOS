297c80b8d64449c35eb175dbf9451ee0be03f042
FlareCoding <albert.slepak@gmail.com>
2025-03-03 13:35:49 -0500

    implemented a secondary 'console' mode for the graphics manager to display kernel log messages instead of the regular compositor view

--- userland/apps/gfx_manager/screen_manager.h
+++ userland/apps/gfx_manager/screen_manager.h
@@ -12,6 +12,11 @@ struct user_session {
     stella_ui::window_base* window;
 };
 
+enum class screen_manager_mode {
+    console,
+    compositor
+};
+
 class screen_manager {
 public:
     screen_manager();
@@ -19,13 +24,19 @@ public:
 
     bool initialize();
 
+    void set_background_color(const stella_ui::color& color);
+
     void begin_frame();
     void end_frame();
 
     void composite_windows();
     void draw_screen_overlays();
 
+    void draw_kernel_log_console();
+
     void poll_events();
+
+    screen_manager_mode active_mode = screen_manager_mode::console;
 
 private:
     modules::module_base* m_gfx_module;
@@ -34,6 +45,9 @@ private:
     kstl::vector<stella_ui::window_base*> m_window_list;
 
     ipc::mq_handle_t m_incoming_event_queue;
+
+    char* m_console_log_buffer;
+    uint32_t m_max_displayable_console_lines;
 
     bool _create_canvas(psf1_font* font);
     void _draw_mouse_cursor();
