fb4a5ac6d727ff30035309f5b751e826c86d5169
FlareCoding <albert.slepak@gmail.com>
2025-06-08 22:25:55 -0400

    removed redundant debug prints

--- kernel/src/process/elf/elf64_loader.cpp
+++ kernel/src/process/elf/elf64_loader.cpp
@@ -75,10 +75,6 @@ task_control_block* elf64_loader::load_elf(const uint8_t* file_buffer, size_t bu
     }
 
     _log_info("ELF loaded successfully, task created.");
-
-    // For debugging purposes
-    dbg_print_vma_regions(&task->mm_ctx, task->name);
-
     return task;
 }
 
