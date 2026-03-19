9c76e4800898c804c2123f5bc551919ce7bae9c3
FlareCoding <albert.slepak@gmail.com>
2026-03-19 00:49:32 -0700

    fix(dma): flush stale cache lines on DMA page allocation

--- kernel/arch/x86_64/hw/cache.h
+++ kernel/arch/x86_64/hw/cache.h
@@ -10,6 +10,11 @@ namespace cache {
  */
 inline void flush_icache_range(uintptr_t, size_t) {}
 
+/**
+ * No-op on x86_64: cache is snooped by DMA, and UC mappings bypass cache.
+ */
+inline void clean_dcache_poc(uintptr_t, size_t) {}
+
 } // namespace cache
 
 #endif // STELLUX_ARCH_X86_64_HW_CACHE_H
