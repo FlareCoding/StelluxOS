2dc9ddd23daeebde29648032c44c8700e7fbd2b5
FlareCoding <albert.slepak@gmail.com>
2026-03-28 15:41:20 -0700

    feat(tcp): implemented active open connect socket op with SYN_SENT handshake and critical safety fixes

--- userland/apps/tcpecho/src/tcpecho.c
+++ userland/apps/tcpecho/src/tcpecho.c
@@ -10,6 +10,28 @@
 #include <arpa/inet.h>
 #include <errno.h>
 
+static uint32_t parse_ipv4(const char* str) {
+    int field = 0;
+    uint32_t val = 0;
+    uint32_t parts[4] = {0, 0, 0, 0};
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            val = val * 10 + (uint32_t)(str[i] - '0');
+        } else if (str[i] == '.') {
+            if (field >= 3 || val > 255) return 0;
+            parts[field++] = val;
+            val = 0;
+        } else {
+            return 0;
+        }
+    }
+    if (field != 3 || val > 255) return 0;
+    parts[3] = val;
+
+    return (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8) | parts[3];
+}
+
 static void format_ip(uint32_t ip_host, char* buf, size_t buf_size) {
     snprintf(buf, buf_size, "%u.%u.%u.%u",
              (ip_host >> 24) & 0xFF, (ip_host >> 16) & 0xFF,
@@ -81,12 +103,61 @@ static int run_server(uint16_t port) {
     return 0;
 }
 
+static int run_client(uint32_t dst_ip_host, uint16_t port, const char* msg) {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        printf("tcpecho: socket() failed (errno=%d)\r\n", errno);
+        return 1;
+    }
+
+    struct sockaddr_in dst;
+    memset(&dst, 0, sizeof(dst));
+    dst.sin_family = AF_INET;
+    dst.sin_port = htons(port);
+    dst.sin_addr.s_addr = htonl(dst_ip_host);
+
+    char ip_str[16];
+    format_ip(dst_ip_host, ip_str, sizeof(ip_str));
+    printf("tcpecho: connecting to %s:%u...\r\n", ip_str, port);
+
+    if (connect(fd, (struct sockaddr*)&dst, sizeof(dst)) < 0) {
+        printf("tcpecho: connect() failed (errno=%d)\r\n", errno);
+        close(fd);
+        return 1;
+    }
+    printf("tcpecho: connected\r\n");
+
+    size_t msg_len = strlen(msg);
+    printf("tcpecho: sending \"%s\" (%zu bytes)\r\n", msg, msg_len);
+    ssize_t nw = write(fd, msg, msg_len);
+    if (nw < 0) {
+        printf("tcpecho: write() failed (errno=%d)\r\n", errno);
+        close(fd);
+        return 1;
+    }
+
+    char buf[1024];
+    ssize_t nr = read(fd, buf, sizeof(buf) - 1);
+    if (nr > 0) {
+        buf[nr] = '\0';
+        printf("tcpecho: received \"%s\" (%zd bytes)\r\n", buf, nr);
+    } else if (nr == 0) {
+        printf("tcpecho: connection closed by peer\r\n");
+    } else {
+        printf("tcpecho: read() failed (errno=%d)\r\n", errno);
+    }
+
+    close(fd);
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
     setvbuf(stdout, NULL, _IONBF, 0);
 
     if (argc < 3) {
         printf("Usage:\r\n");
         printf("  tcpecho server <port>\r\n");
+        printf("  tcpecho client <ip> <port> [message]\r\n");
         return 1;
     }
 
@@ -97,6 +168,25 @@ int main(int argc, char* argv[]) {
             return 1;
         }
         return run_server(port);
+    }
+
+    if (strcmp(argv[1], "client") == 0) {
+        if (argc < 4) {
+            printf("Usage: tcpecho client <ip> <port> [message]\r\n");
+            return 1;
+        }
+        uint32_t ip = parse_ipv4(argv[2]);
+        if (ip == 0) {
+            printf("tcpecho: invalid IP address '%s'\r\n", argv[2]);
+            return 1;
+        }
+        uint16_t port = (uint16_t)atoi(argv[3]);
+        if (port == 0) {
+            printf("tcpecho: invalid port\r\n");
+            return 1;
+        }
+        const char* msg = (argc >= 5) ? argv[4] : "hello";
+        return run_client(ip, port, msg);
     }
 
     printf("tcpecho: unknown mode '%s'\r\n", argv[1]);
