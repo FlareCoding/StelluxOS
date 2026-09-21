ff71b2cce93622931085b7f7baa92403bfc756db
Albert Slepak <albert@anysphere.co>
2026-09-21 13:41:07 -0700

    refactor(mm): separated the loaded translation root from the kernel root

--- kernel/arch/aarch64/sched/sched.cpp
+++ kernel/arch/aarch64/sched/sched.cpp
@@ -167,7 +167,7 @@ __PRIVILEGED_CODE void arch_init_clone_cpu_context(task* t) {
  * @note Privilege: **required**
  */
 __PRIVILEGED_CODE void arch_post_switch(task* next) {
-    if (paging::get_kernel_pt_root() != next->exec.pt_root) {
+    if (paging::current_pt_root() != next->exec.pt_root) {
         paging::set_kernel_pt_root(next->exec.pt_root);
         paging::flush_tlb_all_local();
     }
